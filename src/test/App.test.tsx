254a0a3f965b5821fc275a897a7d13572de9e4b8
Timc3209 <timc3209@gmail.com>
2020-05-16 18:31:35 -0700

    Updates

--- src/test/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "../App";
-
-test("renders recipe app", () => {
-  const { getByText } = render(<App />);
-  const titleElement = getByText("Recipe App");
-  expect(titleElement).toBeInTheDocument();
-});
