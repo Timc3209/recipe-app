f1426ba5da24cde771daf14aa97b1fcafde9a0d7
Timc3209 <timc3209@gmail.com>
2020-05-16 19:02:34 -0700

    update links

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -27,52 +27,45 @@ export default class Recipe extends React.Component<Props, States> {
     isOpen: false,
   };
 
-  toggle = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
+  toggle = () => {
     this.setState({ isOpen: !this.state.isOpen });
-  };
-
-  onEdit = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
-    this.props.editRecipe();
-  };
-
-  onDelete = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
-    this.props.deleteRecipe();
   };
 
   render() {
     const { isOpen } = this.state;
-    const { url, title, ingredients, directions } = this.props;
+    const {
+      url,
+      title,
+      ingredients,
+      directions,
+      editRecipe,
+      deleteRecipe,
+    } = this.props;
 
     return (
       <ListGroupItem className="recipe">
-        <a
-          onClick={this.onDelete}
-          className="delete"
-          href="/#"
+        <span
+          onClick={deleteRecipe}
+          className="recipe-action"
           data-testid="btnDelete"
         >
           <FontAwesomeIcon icon={faTrash} />
-        </a>
-        <a
-          onClick={this.onEdit}
-          className="edit"
-          href="/#"
+        </span>
+        <span
+          onClick={editRecipe}
+          className="recipe-action"
           data-testid="btnEdit"
         >
           <FontAwesomeIcon icon={faEdit} />
-        </a>
-        <a
+        </span>
+        <span
           onClick={this.toggle}
-          className="toggle bg-primary"
-          href="/#"
+          className="recipe-toggle bg-primary"
           data-testid="btnToggle"
         >
           <FontAwesomeIcon icon={isOpen ? faArrowUp : faArrowDown} />
-          <span className="text-white">{title}</span>
-        </a>
+          <span className="recipe-title text-white">{title}</span>
+        </span>
         <Collapse isOpen={isOpen}>
           <div className="content-recipe">
             <ImagePreview src={url} className="image-recipe" />
