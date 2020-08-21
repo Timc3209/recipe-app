4edf62bc64ebc36bd356577b652fec9736bf3e0c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-21 00:15:35 +0000

    Deploy Timc3209/recipe-app to github.com/Timc3209/recipe-app.git:gh-pages

---
+++ precache-manifest.215a7cffac343a1f8ab3d9ebb98b473a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c20dcf20d3872ed241327381975df0",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "a0cbbfcae6b9f8697ad9",
+    "url": "/recipe-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dbdfa8ebb3a0fd898e9f",
+    "url": "/recipe-app/static/css/main.bc53dd14.chunk.css"
+  },
+  {
+    "revision": "a0cbbfcae6b9f8697ad9",
+    "url": "/recipe-app/static/js/2.0edd0899.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/recipe-app/static/js/2.0edd0899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbdfa8ebb3a0fd898e9f",
+    "url": "/recipe-app/static/js/main.ec072277.chunk.js"
+  },
+  {
+    "revision": "6ff38be5769cf5492ba6",
+    "url": "/recipe-app/static/js/runtime-main.dc3db4a1.js"
+  }
+]);
